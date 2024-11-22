b652af0a52eac96f014c76aa6fb5eeaebf43ed49
suresh kumar mettela <mk23ahb@herts.ac.uk>
2024-11-22 09:58:29 +0000

    visualization.R

--- visualization.R
+++ visualization.R
@@ -1,20 +1,13 @@
 library(tidyverse)
 library(ggplot2)
 library(readr)
-
-pdf("visualization1.pdf")
+library(rcompanion)
+pdf("visualization.pdf")
 xyz <- read.csv("chicago_air_pollution.csv")
-x <- xyz$no2tmean2
+x <- xyz$tmpd
 y <- xyz$o3tmean2
-
-ggplot(xyz, aes(x = no2tmean2 , y = o3tmean2) ) +geom_point() +scale_x_continuous(limits = c(0,70))+scale_y_continuous(limits = c(0,70))+ geom_smooth(method="lm" , formula = y ~ x )+labs(title = " Correlation of Nitrogen and Ozone reading in Chicago Air Pollution  ", x = "Nitrogen" , y = "Ozone Reading")
-
-ggplot(xyz, aes(x = x)) +
-  geom_histogram(aes(y =after_stat(density)),
-                 breaks = seq(0, 70, by = 5), 
-                 colour = "black", 
-                 fill = "white") +
-  stat_function(fun = dnorm, args = list(mean = mean(xyz$no2tmean2), sd = sd(xyz$no2tmean2)),colour = "red")+ggtitle( "Histogram of Nitrogen" )+labs( x = "Nitrogen" ,y = "Density")+theme(plot.title = element_text(hjust =0.5))
-
-
+plot(x,y , main = "Correlation of Temprature and Ozone Readings in Chicago Air Quality", xlab = "Temprature Readings", ylab =  "Ozone Readings", col = "grey", xlim = c(0,100))
+abline(lm( y ~ x, data = xyz), col = "blue")
+model = lm(y ~ x,data = xyz)
+plotNormalHistogram(residuals(model),col = "pink",main = "Histogram of Temprature",xlab = "Temprature readings")
 dev.off()
